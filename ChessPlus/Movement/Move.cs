d7e9d833a4bc05645bbf20e42f9afb626f5b4ee6
Zachary Wardley <zachary.wardley@gmail.com>
2024-11-11 15:36:21 -0500

    Restructure Move classes within the project

---
+++ ChessPlus/Movement/Move.cs
@@ -0,0 +1,20 @@
+﻿using ChessPlus.Positions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessPlus.Movement
+{
+    internal abstract class Move
+    {
+        public Position From { get; set; }
+        public Position To { get; set; }
+
+        public Move(Position from, Position to)
+        {
+            From = from;
+            To = to;
+        }
+    }
