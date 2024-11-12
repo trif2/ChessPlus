6ff6f96f53a8b672674545aa83dd59d03aef6beb
Zachary Wardley <zachary.wardley@gmail.com>
2024-11-12 12:11:31 -0500

    Add AddDirection(s) methods to Positions, remove unnecessary methods from HexDirections

--- ChessPlus/Direction/HexDirections.cs
+++ ChessPlus/Direction/HexDirections.cs
@@ -10,15 +10,5 @@ namespace ChessPlus.Direction
         public static readonly (int Q, int R, int S) DownRight = (1, -1, 0);
         public static readonly (int Q, int R, int S) UpRight = (1, 0, -1);
         public static readonly (int Q, int R, int S) DownLeft = (-1, 0, 1);
-
-        public static (int Q, int R, int S) MultiplyDirection((int Q, int R, int S) direction, int scalar)
-        {
-            return (direction.Q * scalar, direction.R * scalar, direction.S * scalar);
-        }
-        public static HexPosition AddPos(HexPosition position, (int Q, int R, int S) direction)
-        {
-            HexPosition newPosition = new HexPosition(position.Q + direction.Q, position.R + direction.R, position.S + direction.S);
-            return newPosition;
-        }
     }
 }
--- ChessPlus/Position/Positions.cs
+++ ChessPlus/Position/Positions.cs
@@ -2,26 +2,62 @@
 {
     internal abstract class Position
     {
-        public abstract int Y { get; }
-        public abstract int X { get; }
-        public abstract int Q { get; }
-        public abstract int R { get; }
-        public abstract int S { get; }
+        public abstract int Y { get; set; }
+        public abstract int X { get; set; }
+        public abstract int Q { get; set; }
+        public abstract int R { get; set; }
+        public abstract int S { get; set; }
     }
     internal class ClassicPosition(int y, int x) : Position
     {
-        public override int Y { get; } = y;
-        public override int X { get; } = x;
-        public override int Q => throw new NotSupportedException();
-        public override int R => throw new NotSupportedException();
-        public override int S => throw new NotSupportedException();
+        public override int Y { get; set; } = y;
+        public override int X { get; set; } = x;
+        public override int Q
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+        public override int R
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+        public override int S
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+        public ClassicPosition AddDirection((int Y, int X) direction, int scalar)
+        {
+            int newY = Y + direction.Y * scalar;
+            int newX = X + direction.X * scalar;
+            return new ClassicPosition(newY, newX);
+        }
+        public ClassicPosition AddDirections((int Y, int X) direction1, int scalar1, (int Y, int X) direction2, int scalar2)
+        {
+            int newY = Y + direction1.Y * scalar1 + direction2.Y * scalar2;
+            int newX = X + direction1.X * scalar1 + direction2.X * scalar2;
+            return new ClassicPosition(newY, newX);
+        }
     }
     internal class HexPosition(int q = 0, int r = 0, int s = 0) : Position
     {
-        public override int Q { get; } = q;
-        public override int R { get; } = r;
-        public override int S { get; } = s;
-        public override int Y => throw new NotSupportedException();
-        public override int X => throw new NotSupportedException();
+        public override int Q { get; set; } = q;
+        public override int R { get; set; } = r;
+        public override int S { get; set; } = s;
+        public override int Y
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+        public override int X
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+        public HexPosition AddDirection((int Q, int R, int S) direction, int scalar)
+        {
+            return new HexPosition(Q + direction.Q, R + direction.R, S + direction.S);
+        }
     }
 }
