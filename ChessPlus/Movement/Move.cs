56d4a0ba87759ac240a53392b7bb4728d59cd318
Zachary Wardley <zachary.wardley@gmail.com>
2024-11-19 08:50:41 -0500

    Add ToString method for Move

--- ChessPlus/Movement/Move.cs
+++ ChessPlus/Movement/Move.cs
@@ -17,5 +17,43 @@ namespace ChessPlus.Movement
             From = from;
             To = to;
         }
+
+        public override string ToString()
+        {
+            return PositionToString(From) + PositionToString(To);
+        }
+        private string PositionToString(Position pos)
+        {
+            string letter = "";
+            switch (pos.X)
+            {
+                case 0:
+                    letter = "a";
+                    break;
+                case 1:
+                    letter = "b";
+                    break;
+                case 2:
+                    letter = "c";
+                    break;
+                case 3:
+                    letter = "d";
+                    break;
+                case 4:
+                    letter = "e";
+                    break;
+                case 5:
+                    letter = "f";
+                    break;
+                case 6:
+                    letter = "g";
+                    break;
+                case 7:
+                    letter = "h";
+                    break;
+            }
+            int number = 8 - pos.Y;
+            return letter + number.ToString();
+        }
     }
 }
