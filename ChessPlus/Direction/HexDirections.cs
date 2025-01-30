d40d174d0e9b53577c6416cb4216e299ac0ecd7b
Zachary Wardley <zachary.wardley@gmail.com>
2025-01-30 11:55:56 -0500

    Fix AddDirection for HexPosition, add FullRight direction

--- ChessPlus/Direction/HexDirections.cs
+++ ChessPlus/Direction/HexDirections.cs
@@ -2,11 +2,13 @@
 {
     static class HexDirections
     {
-        public static readonly (int Q, int R, int S) Up = (0, 1, -1);
-        public static readonly (int Q, int R, int S) Down = (0, -1, 1);
-        public static readonly (int Q, int R, int S) UpLeft = (-1, 1, 0);
-        public static readonly (int Q, int R, int S) DownRight = (1, -1, 0);
-        public static readonly (int Q, int R, int S) UpRight = (1, 0, -1);
-        public static readonly (int Q, int R, int S) DownLeft = (-1, 0, 1);
+        public static readonly (int Q, int R, int S) Up = (0, -1, 1);
+        public static readonly (int Q, int R, int S) Down = (0, 1, -1);
+        public static readonly (int Q, int R, int S) UpLeft = (-1, 0, 1);
+        public static readonly (int Q, int R, int S) UpRight = (1, -1, 0);
+        public static readonly (int Q, int R, int S) DownRight = (1, 0, -1);
+        public static readonly (int Q, int R, int S) DownLeft = (-1, 1, 0);
+
+        public static readonly (int Q, int R, int S) FullRight = (2, -1, -1);
     }
 }
